e233ac17c86fc18e494fdceeaf087dd395e0930e
Advitiay Anand <advitiay.a@gmail.com>
2021-10-09 22:17:48 +0530

    Modify Loader

--- lib/widgets/loader.dart
+++ lib/widgets/loader.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:milestones/shared/constants.dart';
 
 class Loader extends StatelessWidget {
   const Loader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const CircularProgressIndicator();
+    return Container(
+      color: colorBackground,
+      child: const Center(
+        child: CircularProgressIndicator(
+          color: colorPrimary,
+        ),
+      ),
+    );
   }
 }
