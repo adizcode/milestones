aec9ffacfd1df0958195d75a05bf7720df129e22
Advitiay Anand <advitiay.a@gmail.com>
2021-10-14 07:15:58 +0530

    Add floating action button on top of milestones

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,13 +7,12 @@ import 'package:milestones/services/auth.dart';
 import 'package:milestones/services/database.dart';
 import 'package:milestones/shared/constants.dart';
 import 'package:milestones/shared/milestones_snackbar.dart';
+import 'package:milestones/shared/validators.dart';
+import 'package:milestones/widgets/frosted_glass_filter.dart';
 import 'package:milestones/widgets/milestones_scaffold.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-
-// Constant
-const int _totalMilestonesCount = 10;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -23,7 +22,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late final DatabaseService db;
+  final _formKey = GlobalKey<FormState>();
+  String _milestoneTask = '';
+
+  PanelController panelController = PanelController();
+  DatabaseService? db;
 
   @override
   void didChangeDependencies() {
@@ -34,7 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Milestone>>(
-      stream: db.milestones,
+      stream: db!.milestones,
       builder: (context, snapshot) {
         return MilestonesScaffold(
           onActionPressed: () async {
@@ -43,23 +46,43 @@ class _HomeScreenState extends State<HomeScreen> {
           actionIcon: Icons.logout,
           actionLabel: 'Logout',
           child: SlidingUpPanel(
-            panelBuilder: (scrollController) => _milestonesWidgetBuilder(
+            controller: panelController,
+            panelBuilder: (scrollController) => milestonesWidgetBuilder(
               scrollController,
               snapshot.data ?? [],
             ),
-            backdropEnabled: true,
-            backdropOpacity: 0.1,
             body: MilestonesProgressWidget(
-              totalMilestonesCount: _totalMilestonesCount,
+              totalMilestonesCount: totalMilestonesCount,
               currentMilestonesCount: snapshot.data?.length ?? 0,
             ),
-            minHeight: 12.h,
+            collapsed: ElevatedButton(
+              child: const Text('View Milestones'),
+              onPressed: () => panelController.open(),
+              style: ElevatedButton.styleFrom(
+                primary: colorPrimary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(borderRadius),
+                    topRight: Radius.circular(borderRadius),
+                  ),
+                ),
+                elevation: 0,
+                textStyle: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            minHeight: 8.h,
             maxHeight: 85.h,
+            parallaxEnabled: true,
+            parallaxOffset: 1,
+            backdropEnabled: true,
+            backdropOpacity: 0.1,
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(borderRadius),
               topRight: Radius.circular(borderRadius),
             ),
-            color: colorPrimary.withOpacity(0.65),
+            color: colorPrimary.withOpacity(0.5),
             boxShadow: null,
           ),
         );
@@ -67,13 +90,13 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _milestonesItemBuilder(BuildContext context, Milestone milestone) {
+  Widget milestonesItemBuilder(BuildContext context, Milestone milestone) {
     // Add Animated Padding
     return Dismissible(
       key: Key(milestone.id),
       onDismissed: (direction) async {
         // Delete milestone
-        await db.deleteMilestone(milestone.id);
+        await db!.deleteMilestone(milestone.id);
 
         showMilestonesSnackBar(
           context: context,
@@ -92,7 +115,7 @@ class _HomeScreenState extends State<HomeScreen> {
               milestone.task,
               style: GoogleFonts.poppins(
                 fontWeight: FontWeight.w500,
-                fontSize: 14.sp,
+                fontSize: 13.sp,
                 color: colorPrimary,
               ),
               textAlign: TextAlign.center,
@@ -124,15 +147,95 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _milestonesWidgetBuilder(
+  Widget milestonesWidgetBuilder(
       ScrollController scrollController, List<Milestone> milestones) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.w),
-      child: ListView.builder(
-        controller: scrollController,
-        itemCount: milestones.length,
-        itemBuilder: (context, index) =>
-            _milestonesItemBuilder(context, milestones[index]),
+        padding: EdgeInsets.symmetric(horizontal: 8.w),
+        child: Stack(
+          children: [
+            ListView.builder(
+              controller: scrollController,
+              itemCount: milestones.length,
+              itemBuilder: (context, index) =>
+                  milestonesItemBuilder(context, milestones[index]),
+            ),
+            Align(
+              alignment: const Alignment(0, 0.85),
+              child: FloatingActionButton(
+                onPressed: () => showDialog(
+                  context: context,
+                  builder: milestonesDialogBuilder,
+                ),
+                child: const Icon(Icons.add),
+                backgroundColor: colorAccentLight,
+              ),
+            )
+          ],
+        ));
+  }
+
+  Widget milestonesDialogBuilder(BuildContext context) {
+    return FrostedGlassFilter(
+      sigmaX: 1,
+      sigmaY: 1,
+      child: Dialog(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius / 2),
+        ),
+        elevation: 2,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10.w),
+          height: 28.h,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  initialValue: _milestoneTask,
+                  onChanged: (task) => _milestoneTask = task,
+                  validator: emptyTextValidator,
+                  decoration: textInputDecoration.copyWith(
+                    labelText: 'What did you achieve?',
+                    labelStyle: GoogleFonts.poppins(
+                      color: colorPrimary,
+                      fontSize: 12.sp,
+                    ),
+                  ),
+                  cursorColor: colorPrimary,
+                ),
+                SizedBox(
+                  height: 1.5.h,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    // Validate form field
+                    if (_formKey.currentState?.validate() ?? false) {
+                      // Add milestone
+                      await db!.setMilestone(
+                        {'task': _milestoneTask},
+                      );
+
+                      // Close dialog
+                      Navigator.pop(context);
+
+                      // Reset form field
+                      _milestoneTask = '';
+                    }
+                  },
+                  child: const Text(
+                    'Add Milestone',
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size.fromHeight(5.h),
+                    primary: colorPrimary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
